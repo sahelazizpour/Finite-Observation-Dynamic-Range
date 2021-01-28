e1fad186408820f86a6abaf1feadd9f74d1e465b
Sahel Azizpour <sahel.azizpour@gmail.com>
2021-01-28 16:04:10 +0100

    n realizations in one job

--- functions.py
+++ functions.py
@@ -156,3 +156,59 @@ def do_realization_save_window_average(path,N,k,alpha,h_list,lambda_list,n_stati
     for i in range(len(window_size)):
         np.savez_compressed(path + '/window'+str(i)+'_lambda_='+str(lambda_)+'_n_h='+str(n_h)+'_realization=' + str(n), mean_response=mean_response[i])
 
+def do_n_realizations_save_window_average(path,N,k,alpha,n_realization,n_stationary,n_data,init_activity,input_type,window_size,lambda_,h):
+    '''
+    :param n_stationary:
+    :param n_data: number of minimum response mean samples to compute
+    :param window_size: the observation window sizes to average network responses over
+    :param path0: path to save data
+    :param h: input intensity
+    '''
+
+
+    print('lambda=' + str(lambda_) + ', h=' + str(h) )
+    sp.random.seed()
+    n_max=1000000           # maximum number of response mean samples to save
+    chunk_size=100000      # produced trajectory in each round of the loop
+    tau=1000               # ~ auto correlation time
+    gamma = lambda_ / (k * (1 - 2 * alpha))     #connection weight
+    p_input = 1 - np.exp(-h)        #probability of external Poissonian input
+
+    for n in range(n_realization):
+        sparseA=draw_connections(lambda_, k, N, alpha, gamma)
+        pickle.dump(sparseA, open(path + '/sparseA_realization='+str(n), 'wb'))
+
+        mean_response=[[]]
+        for i in range(len(window_size)-1):
+            mean_response.append([])
+
+        s = np.zeros(N)  # activity vector
+        # generate initial activity
+        g = np.int(N * init_activity)
+        c = np.random.permutation(N)
+        ind = c[0:g]
+        s[ind] = 1
+        # run simulation until it reaches steady state
+        for i in range(n_stationary):
+            s=do_one_timestep(input_type,N,sparseA,s,p_input)
+        # t = time.time()
+        while len(mean_response[-1])<n_data:
+            s_avg_temp = []
+            print('n_data=',len(mean_response[-1]))
+            # produce trajectories of length chunck_size
+            for step in range(chunk_size):
+                s = do_one_timestep(input_type, N, sparseA, s, p_input)
+                s_avg_temp.append(np.sum(s) * (1 / N))
+
+            #average over window sizes
+            for i in range(len(window_size)):
+                if len(mean_response[i]) < n_max:
+                    if window_size[i]<tau:
+                        groups = [s_avg_temp[x:x + window_size[i]] for x in np.arange(0,int(len(s_avg_temp) / tau))*tau]
+                    else:
+                        groups = [s_avg_temp[x:x + window_size[i]] for x in np.arange(0, int(len(s_avg_temp) / window_size[i])) * window_size[i]]
+                    mean_response[i] = np.append(mean_response[i], [sum(group) / len(group) for group in groups])
+        # print(time.time() - t)
+
+        for i in range(len(window_size)):
+            np.savez_compressed(path + '/window'+str(i)+'_lambda_='+str(lambda_)+'_logh='+str(np.round(np.log10(h),3))+'_realization=' + str(n), logh=np.log10(h), mean_response=mean_response[i])
--- network_simulation_window_based.py
+++ network_simulation_window_based.py
@@ -10,36 +10,34 @@ import sys
 import time
 warnings.filterwarnings("ignore")
 
-n_lambda = int(sys.argv[1])
-n_h = int(sys.argv[2])
-n_realization = int(sys.argv[3])
+lambda_ = float(sys.argv[1])
+h = float(sys.argv[2])
 
 sp.random.seed()
-path_to_save =  'data/blah/'
+path_to_save =  'FODR/'
 
 
-lambda_list=[0.9999]                                 # list of lambda values
-h_list = np.logspace(-8,2,400).tolist()              # list of input intensities
+n_realization=10
+# h_list = np.logspace(-7,2,270).tolist()              # list of input intensities
 alpha = 0                                            # fraction of inhibitory neurons
 N=10000                                               # number of neurons
 k=20                                                 # in/out connectivity degree (choose multiples of 5 or change the function "draw_connections)
 input_type='multiplicative'                          # input can be added multiplicatively or additively
 homogeneity = 1                                      # homogeneity of network graph
 hyperregularity=1                                    # hyperregularity of network graph
-n_data = 1                                     # minimum number of mean responses computed at each realization
+n_data = 100000                                     # minimum number of mean responses computed at each realization
 init_activity=0.01                                   # fraction of initial active neurons
-n_stationary=10                                    # time steps after which activity becomes stationary
+n_stationary=10000                                    # time steps after which activity becomes stationary
 window_size=np.array([1e1,1e2,1e3,1e4],dtype=np.int) #window sizes for computing the mean response, in ascending order, factors of 10
 
 #save parameters
-pickle.dump([lambda_list, h_list, alpha,N, k, homogeneity, input_type, hyperregularity,n_data, init_activity, n_stationary,
+pickle.dump([ alpha,N, k, homogeneity, input_type, hyperregularity,n_data, init_activity, n_stationary,
              window_size], open(path_to_save +'/parameters', 'wb'))
 
-
-func = partial(functions.do_realization_save_window_average,path_to_save, N, k, alpha, h_list,lambda_list, n_stationary, n_data,
+func = partial(functions.do_n_realizations_save_window_average,path_to_save, N, k, alpha,n_realization, n_stationary, n_data,
                init_activity, input_type,window_size)
 # t=time.time()
-func(n_lambda,n_h,n_realization)
+func(lambda_,h)
 # print(time.time()-t)
 
 
