010c8404f6c9d30ffdcff804fe93484d6bf262b5
Sahel Azizpour <sahel.azizpour@gmail.com>
2021-02-12 12:18:52 +0100

    removed the loop over realizations

--- network_simulation_window_based.py
+++ network_simulation_window_based.py
@@ -13,18 +13,15 @@ warnings.filterwarnings("ignore")
 lambda_ = float(sys.argv[1])
 n_h = int(sys.argv[2])
 n = int(sys.argv[3])
+path_to_save =  'FODR/'
 
 # lambda_list=1-np.logspace(-4,0,9)
 # lambda_=lambda_list[n_lambda]
-
 logh_list = np.arange(-7,-0.5,0.05).tolist()+ np.arange(-0.5,1.5,0.025).tolist()       # list of input intensities
 h_list=np.power(10,logh_list)
 h=h_list[n_h]
 
 sp.random.seed()
-path_to_save =  'FODR/'
-
-
 n_realization=10
 alpha = 0                                            # fraction of inhibitory neurons
 N=10000                                               # number of neurons
@@ -44,18 +41,17 @@ pickle.dump([ alpha,N, k, homogeneity, input_type, hyperregularity,n_data, init_
 
 
 #produce adjacency matrix
-try:
-    sparseA_list=pickle.load(open(path_to_save +'/sparseAlist_lambda='+str(lambda_), 'rb'))
-except:
-    sparseA_list=[]
-    for j in range(n_realization):
-        sparseA_list.append(functions.draw_connections(lambda_, k, N, alpha, gamma))
-    pickle.dump(sparseA_list, open(path_to_save + '/sparseAlist_lambda='+str(lambda_) , 'wb'))
+# try:
+#     sparseA_list=pickle.load(open(path_to_save +'/sparseA_list'+str(lambda_), 'rb'))
+# except:
+#     sparseA_list=[]
+#     for i in range(n_realization):
+#         sparseA_list.append(functions.draw_connections(lambda_, k, N, alpha, gamma))
+#     pickle.dump(sparseA_list, open(path_to_save + '/sparseA_lambda='+str(lambda_), 'wb'))
 
-sparseA=sparseA_list[n]
+sparseA=pickle.load(open(path_to_save +'/sparseA_lambda='+str(lambda_)+'_realization='+str(n), 'rb'))
 func = partial(functions.do_realization_save_window_average,path_to_save,N,n_stationary,n_data,init_activity,input_type,window_size,sparseA)
-# t=time.time()
 func(lambda_,h,n)
-# print(time.time()-t)
+
 
 
