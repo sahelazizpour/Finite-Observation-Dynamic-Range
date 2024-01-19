b47304ffbd8ad1eac667815283fe6758c2819d23
Johannes Zierenberg <johannes.zierenberg@gmail.com>
2024-01-19 17:30:57 +0100

    save and load nn func approximation

--- 0_update/src/approximation.py
+++ 0_update/src/approximation.py
@@ -2,6 +2,10 @@
 ##################
 import numpy as np
 from scipy import stats, signal, optimize
+import os
+import pickle
+import inspect
+import h5py
 
 
 def fit_beta_distribution(data, delta, seed=1234):
@@ -26,6 +30,7 @@ import torch.optim as optim
 from sklearn.preprocessing import MinMaxScaler
 from tqdm import tqdm
 
+
 class DenseNN(nn.Module):
     """
     This class implements a fully connected neural network with a variable number of hidden layers.
@@ -44,6 +49,7 @@ class DenseNN(nn.Module):
     act: torch.nn.Module
         Activation function (default nn.Tanh()).
     """
+
     def __init__(self, input_size, hidden_sizes, output_size, act=nn.Tanh()):
         super().__init__()
         # create activation function
@@ -57,7 +63,7 @@ class DenseNN(nn.Module):
     def forward(self, x):
         for layer in self.hidden:
             x = self.act(layer(x))
-            
+
         return self.output(x)
 
 
@@ -88,89 +94,109 @@ class FunctionApproximation:
         List of output variables.
     """
 
-    def __init__(self, input_names, output_names, model=None, verbose=False):
-        # assert that input and output lists do not share any elements
-        assert len(set(input_names).intersection(set(output_names))) == 0
-        self.input_names = input_names
-        self.output_names = output_names
+    def __init__(self, verbose=False, **kwargs):
+        self.verbose = verbose
+        if "params" in kwargs:
+            self.params = kwargs["params"]
+        else:
+            self.params = None
+        if "filename" in kwargs:
+            filename = kwargs["filename"]
+            if self.verbose:
+                print(
+                    f"Loading function approximation from {filename} and ignore additional arguments!"
+                )
+            self.load(filename)
+        else:
+            self.init(**kwargs)
+
+    def init(self, **kwargs):
+        self.input_names = kwargs["input"]
+        self.output_names = kwargs["output"]
+        assert len(set(self.input_names).intersection(set(self.output_names))) == 0
         self.input_dim = len(self.input_names)
         self.output_dim = len(self.output_names)
         self.map_input = [lambda x: x for i in range(self.input_dim)]
         self.map_output = [lambda x: x for i in range(self.output_dim)]
         self.map_output_inv = [lambda x: x for i in range(self.output_dim)]
-        self.verbose = verbose  
-        self.device = torch.device(
-            "cuda"
-            if torch.cuda.is_available()
-            else "mps"
-            if torch.backends.mps.is_available()
-            else "cpu"
-        )
-        if self.verbose: 
-            print("Using device: {}".format(self.device))
 
-        if model==None:
-            self.model = DenseNN(input_size=self.input_dim, hidden_sizes=[42, 42, 42], output_size=self.output_dim)
-
-        if verbose:
+        if "model" in kwargs:
+            self.model = kwargs["model"]
+        else:
+            if self.verbose:
+                print(
+                    "Initializing default model (choose own model with kwargs `model`)."
+                )
+            self.model = DenseNN(
+                input_size=self.input_dim,
+                hidden_sizes=[42, 42, 42],
+                output_size=self.output_dim,
+            )
+        if self.verbose:
             print(self.model)
-        
+
         # Attention: scaler has to match the activation function:
         # tanh: [-1,1]
-        # sigmoid: [0,1]    
+        # sigmoid: [0,1]
         self.X_scaler = MinMaxScaler(feature_range=(-1, 1))
         self.Y_scaler = MinMaxScaler(feature_range=(-1, 1))
-        # for now: throw error if self.model.act is not nn.Tanh()        
 
-    def change_map(self, label, func, func_inv=None):
+        if self.verbose:
+            print(
+                "Next steps:\n>> func.set_map() # optimize parameter distribution over activation function domain (input) and range (output) \n>> func.train() # train neural network model\n>> func.save(filename) # save function approximation to file"
+            )
+
+    def set_map_to_NN(self, label, func):
         """
-            Specifies a mapping for parameter `name` from either input or output to the NN.
-            The pipeline is thus as follows: input -> func -> NN -> func_inv -> output
+        Specifies a mapping for parameter `name` from either input or output to the NN.
+        The pipeline is thus as follows: input -> func -> NN -> func_inv -> output
         """
-        # find index of name in input_names
         if label in self.input_names:
             i = self.input_names.index(label)
             self.map_input[i] = func
         elif label in self.output_names:
             i = self.output_names.index(label)
             self.map_output[i] = func
-            if func_inv is None:
-                raise ValueError("Please specify an inverse mapping for the output!")
-            self.map_output_inv[i] = func_inv
     
-    def __call__(self, x):
+    def set_map_from_NN(self, label, func):
         """
-            Returns the function approximation at `x`.
+        Specifies a mapping for parameter `name` from either input or output to the NN.
+        The pipeline is thus as follows: input -> func -> NN -> func_inv -> output
         """
-        if self.model.training:
-            raise ValueError("FunctionApproximation has not finished training yet!")
-        X = x.copy()
-        for i in range(self.input_dim):
-            X[:, i] = self.map_input[i](x[:, i])
-        
-        X_scaled = self.X_scaler.transform(X)
-        Y_scaled = self.model(torch.from_numpy(X_scaled).float()).detach().numpy()
-        Y = self.Y_scaler.inverse_transform(Y_scaled)
-        y = Y.copy()
-        for i in range(self.output_dim):
-            y[:, i] = self.map_output_inv[i](Y[:, i])
-        return y
+        if label in self.output_names:
+            i = self.output_names.index(label)
+            self.map_output_inv[i] = func
+        elif label in self.input_names:
+            raise ValueError("Inverse mapping only required for output variables!")
+
 
     def train(self, dataframe, epochs=1000, lr=0.001):
         """
         Trains the neural network.
         """
-        # retrieve training data from dataframe into numpy arrays
+        # select suitable device for training
+        device = torch.device(
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps"
+            if torch.backends.mps.is_available()
+            else "cpu"
+        )
+        if self.verbose:
+            print("Device for training: {}".format(device))
+
+        # extract training data from dataframe into numpy arrays
         Xs = dataframe[self.input_names].values
         Ys = dataframe[self.output_names].values
 
-        # map inputs and outputs so that data is equidistant (e.g. into logspace if data comes from a log-normal distribution)
+        # map inputs and outputs so that data is equidistant
+        # (e.g. into logspace if data comes from a log-normal distribution)
         for i in range(self.input_dim):
             Xs[:, i] = self.map_input[i](Xs[:, i])
         for i in range(self.output_dim):
             Ys[:, i] = self.map_output[i](Ys[:, i])
 
-        # rescale data into [-1,1] range    
+        # rescale data into [-1,1] range (this restricts the fit to the range of the training data!!)
         Xs = self.X_scaler.fit_transform(Xs)
         Ys = self.Y_scaler.fit_transform(Ys)
 
@@ -178,17 +204,18 @@ class FunctionApproximation:
         X_tensor = torch.from_numpy(Xs).float()
         Y_tensor = torch.from_numpy(Ys).float()
         if self.verbose:
-            print(f"Training data with input shape {X_tensor.shape} and output shape {Y_tensor.shape}.")
+            print(
+                f"Training data with input shape {X_tensor.shape} and output shape {Y_tensor.shape}."
+            )
 
         # Adam and MSE Loss
         optimizer = optim.Adam(self.model.parameters(), lr=0.005)
-        loss_fn = nn.MSELoss(reduction='mean')
+        loss_fn = nn.MSELoss(reduction="mean")
 
         # TODO in each epoch make a random sample of the data?
-        # TODO: check why this is slower than the reference implementation that should be identical but also result is different ...
         history_loss = []
         for epoch in tqdm(range(epochs), desc="Training"):
-            #permutation = torch.randperm(len(self.data))
+            # permutation = torch.randperm(len(self.data))
             Y_pred = self.model(X_tensor)
             loss = loss_fn(Y_pred, Y_tensor)
             history_loss.append(loss.item())
@@ -198,12 +225,102 @@ class FunctionApproximation:
             optimizer.step()
             # zero the parameter gradients
             optimizer.zero_grad()
-           
+
         self.model.train(False)
         if self.verbose:
-            print("Training complete; left training mode for faster evaluation! To re-enter training mode call `func.model.train(True)`.")
+            print(
+                "Training complete; left training mode for faster evaluation! To re-enter training mode call `func.model.train(True)`."
+            )
 
         return history_loss
-            
 
- 
+    def save(self, filename):
+        """
+        Saves the function approximation to a pickle file.
+        """
+
+        # step-by-step save all the relevant objects for running the function approximation (not training!)
+        def to_string(lambda_function):
+            # getsource returns "func.set_map_to_NN('lambda', lambda x: f(x) )\n" or "func.set_map_from_NN('lambda', lambda x: f_inv(x) )"
+            # we want to return "lambda x: f(x)"
+            return inspect.getsource(lambda_function).split(",")[1][:-2]
+
+        # create datastructure that can be iteratively passed to pickle.dump
+        data = {}
+        data["params"] = self.params
+        data["input_names"] = self.input_names
+        data["output_names"] = self.output_names
+        # convert lambda functions to strings
+        data["map_input"] = [to_string(m) for m in self.map_input]
+        data["map_output"] = [to_string(m) for m in self.map_output]
+        data["map_output_inv"] = [to_string(m) for m in self.map_output_inv]
+        data["X_scaler"] = self.X_scaler
+        data["Y_scaler"] = self.Y_scaler
+        data["model"] = self.model
+        
+        with open(filename, "wb") as f:
+            pickle.dump(data, f)    
+
+
+    def load(self, filename):
+        """
+        Loads class objects from a pickle file.
+        """
+        data = pickle.load(open(filename, "rb"))
+        self.params = data["params"]
+        self.input_names = data["input_names"]
+        self.output_names = data["output_names"]
+        self.input_dim = len(self.input_names)
+        self.output_dim = len(self.output_names)
+        # convert strings back to lambda functions
+        self.map_input = [eval(m) for m in data["map_input"]]
+        self.map_output = [eval(m) for m in data["map_output"]]
+        self.map_output_inv = [eval(m) for m in data["map_output_inv"]]
+        self.X_scaler = data["X_scaler"]
+        self.Y_scaler = data["Y_scaler"]
+        self.model = data["model"]
+        self.model.train(False)
+
+    def __call__(self, *x):
+        """
+        Returns the approximation of y1,y2, ... = func(x1,x2,...) where x1,x2 are the arguments in the same order as specified in `func.input_names` and y1, y2, ... are the outputs in the same order as specified in `func.output_names`.
+
+        xi currently have to be lists or numpy arrays of the same length.
+        """
+        if self.model.training:
+            raise ValueError("FunctionApproximation has not finished training yet!")
+
+        x = list(x)
+
+        if len(x) != self.input_dim:
+            raise ValueError(
+                f"Expected {self.input_dim} input values, but got {len(x)}!"
+            )
+
+        # iterate over inputs to transform scalars into arrays and apply mapping
+        for i in range(self.input_dim):
+            if np.isscalar(x[i]):
+                x[i] = np.array([x[i]])
+            else:
+                x[i] = np.array(x[i])
+
+            # apply mapping
+            x[i] = self.map_input[i](x[i])
+
+        # stack input list into one array
+        X = np.stack(x, axis=1)
+
+        X_scaled = self.X_scaler.transform(X)
+        Y_scaled = self.model(torch.from_numpy(X_scaled).float()).detach().numpy()
+        Y = self.Y_scaler.inverse_transform(Y_scaled)
+
+        # unstack output array into separate arrays
+        y = [Y[:, i] for i in range(self.output_dim)]
+        # apply inverse mapping
+        for i in range(self.output_dim):
+            y[i] = self.map_output_inv[i](y[i])
+            # if output is a scalar, return scalar
+            if len(y[i]) == 1:
+                y[i] = y[i][0]
+
+        return tuple(y)
