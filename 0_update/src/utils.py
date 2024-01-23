effa30cd283086b0a28ddeac4e64db8c161c5e04
Johannes Zierenberg <johannes.zierenberg@gmail.com>
2024-01-23 13:36:24 +0100

    workflow seems complete

--- 0_update/src/utils.py
+++ 0_update/src/utils.py
@@ -1,6 +1,25 @@
 import sqlite3
 import pandas as pd
 
+# quick fetch of data from database tables
+def fetch_from_database(con, cur, table, params):
+    """
+        fetch from table
+
+        Parameters
+        ----------
+        con : sqlite3 connection object
+            connection object
+        cur : sqlite3 cursor object
+            cursor object
+        table : str
+            table name
+        params : dict
+            dictionary of parameters (keys and values) wheren keys has to match the column names of the table!
+    """
+    cur.execute(f"SELECT * FROM {table} WHERE {' AND '.join([f'{key}=?' for key in params.keys()])}", list(params.values()))
+    return cur.fetchall()
+
 # for quick insertion of data into database tables
 def insert_into_database(con, cur, table, params):
     """
