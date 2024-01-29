5f8109e7fd0e510097bd7f582789f14afccbc0d5
Johannes Zierenberg <johannes.zierenberg@gmail.com>
2024-01-29 15:56:11 +0100

    seeds 1000-1003

--- 0_update/1-2_submit_to_cluster.sh
+++ 0_update/1-2_submit_to_cluster.sh
@@ -9,12 +9,11 @@
 #$ -e /scratch02.local/johannes/projects/sahel_finite-observation-dynamic-range/log/
 #$ -t 1-595
 
-
 conda activate finite-observation
 exe="/data.nst/johannes/projects/sahel_finite-observation-dynamic-range/0_update/1_run_simulation.py"
 file_db="/data.nst/johannes/projects/sahel_finite-observation-dynamic-range/0_update/simulations.db"
 
-seed=1000
+seed=1003
 
 #bash equivalent to numpys hs=np.logspace(-4, 1, 101) going from 1e-4 to 10
 
